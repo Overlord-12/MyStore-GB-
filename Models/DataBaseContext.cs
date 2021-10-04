6cf6c6ee3b227c2fa8e73f693a5be37d11cd4086
@d.argastcev <danya.argastesv.02@mail.ru>
2021-10-04 19:09:27 +0300

    Add migration

--- Models/DataBaseContext.cs
+++ Models/DataBaseContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    class DataBaseContext
+    public class DataBaseContext : DbContext
     {
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options): base(options)
+        {
+
+        }
     }
 }
--- MyStore(GB)/Startup.cs
+++ MyStore(GB)/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyStore_GB_
 {
@@ -25,6 +27,8 @@ namespace MyStore_GB_
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(connection));
             services.AddControllersWithViews();
             services.AddScoped<IEmployeeService,EmployeeService > ();
         }
